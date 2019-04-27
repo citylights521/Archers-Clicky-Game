cab911ebb023533363ac0b81b4f2e25b3964af70
CGibeau <claire.gibeau@outlook.com>
2019-04-27 14:49:35 -0700

    Updates

---
+++ precache-manifest.80ceb47a2b8ec7b3099036e362d0f37a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08cee42de84dac627099519ce4cc83c3",
+    "url": "/Archers-Clicky-Game/index.html"
+  },
+  {
+    "revision": "eb7edf3aa9bf1df31f3c",
+    "url": "/Archers-Clicky-Game/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e88edfece7dec10317f2",
+    "url": "/Archers-Clicky-Game/static/css/main.9e22a64c.chunk.css"
+  },
+  {
+    "revision": "eb7edf3aa9bf1df31f3c",
+    "url": "/Archers-Clicky-Game/static/js/2.b60f2fe3.chunk.js"
+  },
+  {
+    "revision": "e88edfece7dec10317f2",
+    "url": "/Archers-Clicky-Game/static/js/main.5336d308.chunk.js"
+  },
+  {
+    "revision": "e63607991b38802febb0",
+    "url": "/Archers-Clicky-Game/static/js/runtime~main.c4c04887.js"
+  },
+  {
+    "revision": "ff4e14fd76ca7725e9ea04b8c2b41c06",
+    "url": "/Archers-Clicky-Game/static/media/Barry.ff4e14fd.jpg"
+  },
+  {
+    "revision": "18b4a31aa33c96fdc27a4c9de31c952e",
+    "url": "/Archers-Clicky-Game/static/media/Cheryl.18b4a31a.jpg"
+  },
+  {
+    "revision": "9943f2adab3550a9dc5b3df773ef0774",
+    "url": "/Archers-Clicky-Game/static/media/Figgis.9943f2ad.jpg"
+  },
+  {
+    "revision": "ef6a80ea7a6e992d8193f2a5152bd3e4",
+    "url": "/Archers-Clicky-Game/static/media/Jumbotron.ef6a80ea.jpg"
+  },
+  {
+    "revision": "17ff90c868532afbfec948dd56db4542",
+    "url": "/Archers-Clicky-Game/static/media/Krieger.17ff90c8.jpg"
+  },
+  {
+    "revision": "9b417448f2f6307a8ea65faa0dacace4",
+    "url": "/Archers-Clicky-Game/static/media/Lana.9b417448.jpg"
+  },
+  {
+    "revision": "dba6acc86c9e551e0afd8e7f2f111195",
+    "url": "/Archers-Clicky-Game/static/media/Nikolai.Jakov.dba6acc8.png"
+  },
+  {
+    "revision": "fc5175c1ac67640092098987c94051e3",
+    "url": "/Archers-Clicky-Game/static/media/Pam.fc5175c1.jpg"
+  },
+  {
+    "revision": "3cbd8fd292689e85d436d033412421b9",
+    "url": "/Archers-Clicky-Game/static/media/Ray.3cbd8fd2.jpg"
+  },
+  {
+    "revision": "3989394accf9164ca969f1651a74b25f",
+    "url": "/Archers-Clicky-Game/static/media/Ron.Cadillac.3989394a.jpg"
+  },
+  {
+    "revision": "adbd10a0f30f2fc195658f6f87de95ba",
+    "url": "/Archers-Clicky-Game/static/media/Sterling.Archer.adbd10a0.png"
+  },
+  {
+    "revision": "4c94b3061b4698310510d6dd1d783455",
+    "url": "/Archers-Clicky-Game/static/media/Trinette.4c94b306.jpg"
+  },
+  {
+    "revision": "a631e7fe48a067d176c1efe483519833",
+    "url": "/Archers-Clicky-Game/static/media/Woodhouse.a631e7fe.png"
+  }
+]);
